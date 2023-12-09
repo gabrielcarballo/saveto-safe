a864ddd1c6a7c2b3b7587a19171c42dcf834c55d
Gabriel Carballo <gabrielfpcarballo@gmail.com>
2023-12-09 17:10:35 +0100

    Basket component additions

--- src/screens/Basket.tsx
+++ src/screens/Basket.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Image } from "react-native";
-import { StyleSheet, Dimensions, Text } from "react-native";
+import { StyleSheet, Dimensions, Text, Image, View, Button } from "react-native";
 
 const top = require('../../assets/topo.png');
 
@@ -9,8 +8,18 @@ const { width } = Dimensions.get('screen');
 function Basket() {
   return (
     <>
-      <Image source={top} style={styles.top}/>
+      <Image source={top} style={styles.top} />
       <Text style={styles.title}> Basket Details </Text>
+
+      <View style={styles.view}>
+        <Text>Groceries</Text>
+        <Text>Product Name</Text>
+        <Text style={styles.description}>
+          A Description about the product
+        </Text>
+        <Text>Price</Text>
+        <Button title="Add to Basket" />
+      </View>
     </>
   )
 }
@@ -18,7 +27,7 @@ function Basket() {
 const styles = StyleSheet.create({
   top: {
     width: '100%',
-    height: 578/768 * width,
+    height: 578 / 768 * width,
   },
   title: {
     fontSize: 20,
@@ -28,9 +37,23 @@ const styles = StyleSheet.create({
     position: 'absolute',
     width: '100%',
     color: '#fff',
-    
+
+  },
+  view: {
+    flex: 1,
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+
+  },
+  description: {
+    fontSize: 12,
+    fontWeight: "200",
+    fontStyle: "italic"
   }
-});
+}
+);
 
 
 export { Basket };
