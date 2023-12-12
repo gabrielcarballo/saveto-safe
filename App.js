c50189ac9475d403e5d4357dd78916fbc08935b8
Gabriel Carballo <gabrielfpcarballo@gmail.com>
2023-12-12 19:42:11 +0100

    feat: Added SplashScreen usage

--- App.js
+++ App.js
@@ -1,23 +1,45 @@
-import { StatusBar, SafeAreaView, StyleSheet, Text } from 'react-native';
-import React from 'react';
+import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
 import Details from './src/screens/details';
 import { useFonts } from 'expo-font';
 import { Montserrat_400Regular, Montserrat_400Regular_Italic, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
-
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
   const [loaded] = useFonts({
     'Montserrat-Italic': Montserrat_400Regular_Italic,
     'Montserrat-Regular': Montserrat_400Regular,
     'Montserrat-Bold': Montserrat_700Bold,
   });
 
-  if (!loaded) {
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady && loaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady, loaded]);
+
+  if (!appIsReady || !loaded) {
     return null;
   }
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
       <StatusBar />
       <Details />
     </SafeAreaView>
