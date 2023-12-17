4741a9960d6ed580e91f92b79fddb53792a61ce9
Gabriel Carballo <gabrielfpcarballo@gmail.com>
2023-12-17 02:00:44 +0100

    Add selected state and display distance in MarketPricesCard

--- src/screens/details/MarketPricesCard.tsx
+++ src/screens/details/MarketPricesCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, View, Image, TouchableOpacity, Dimensions } from "react-native";
 import MontserratText from '../../components/MontserratText';
 import { marketSearchProps } from "../../mocks/details";
@@ -7,24 +7,27 @@ const imageSize = Dimensions.get('window').width / 8; // adjust this as needed
 
 
 export default function MarketPricesCard({ distance, image, location, name, price }: marketSearchProps) {
+  const [selected, setSelected] = useState();
+  
   let count = 0;
   const handlePress = () => {
     count++;
     console.log(`Pressed on ${name} ${count} times!`);
+    console.log('objeto', selected);
   }
   return (
-    <TouchableOpacity style={styles.container} activeOpacity={0.7} onPress={handlePress}>
+    <TouchableOpacity style={styles.container} activeOpacity={0.9} onPress={handlePress}>
       <View style={styles.imageContainer}>
         <Image source={{ uri: image }} style={styles.image} />
       </View>
       <View style={styles.marketContainer}>
         <MontserratText style={styles.productName}>{name}</MontserratText>
       </View>
-      <View style={styles.glassmorphism}>
-        <MontserratText style={styles.price}>{price}</MontserratText>
+      <View style={styles.priceInfo}>
+        <MontserratText style={styles.price}>$ {price}</MontserratText>
       </View>
       <View style={styles.locationInfo} >
-        <MontserratText style={styles.description}>{distance}</MontserratText>
+        <MontserratText style={styles.distance}>{distance} km</MontserratText>
         <MontserratText style={styles.description}>{location}</MontserratText>
       </View>
     </TouchableOpacity>
@@ -39,8 +42,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   locationInfo: {
-    justifyContent: 'space-between',
     alignItems: 'center',
+    flexWrap: 'wrap',
+    alignSelf: 'center',
   },
   productName: {
     fontSize: 24,
@@ -48,12 +52,14 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: '#C1D5FD',
+    backgroundColor: '#FEEBEB',
     justifyContent: 'space-between',
     padding: 10,
     marginTop: 10,
     marginRight: 10,
     borderRadius: 20,
+    width: 200,
+    height: 220,
   },
   imageContainer: {
     width: imageSize,
@@ -71,18 +77,18 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   marketContainer: {
-    backgroundColor: 'white',
+    // backgroundColor: 'white',
     flexDirection: 'row',
-    borderRadius: 10,
+    // borderRadius: 10,
     justifyContent: 'center',
-    elevation: 5,
-    shadowColor: '#1F2687',
-    shadowOffset: { width: 0, height: 8 },
-    shadowOpacity: 0.37,
-    shadowRadius: 10,
+    // elevation: 5,
+    // shadowColor: '#1F2687',
+    // shadowOffset: { width: 0, height: 8 },
+    // shadowOpacity: 0.37,
+    // shadowRadius: 10,
     marginTop: 20,
   },
-  glassmorphism: {
+  priceInfo: {
     backgroundColor: 'white',
     borderRadius: 10,
     elevation: 5,
@@ -95,6 +101,12 @@ const styles = StyleSheet.create({
   price: {
     fontSize: 30,
     fontWeight: "bold",
-    color: '#1F2687',
+    color: 'black',
   },
+  distance: {
+    fontSize: 16,
+    fontStyle: "italic",
+    color: 'black',
+    fontWeight: "bold",
+  }
 })
